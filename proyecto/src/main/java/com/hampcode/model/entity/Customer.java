e0805d70194bb5ecbafab6f6e6c4384e29e08a84
Henry Antonio Mendoza Puerta <hmendo81@gmail.com>
2019-05-03 16:31:47 -0500

    Creación de Entity Customer y Configuración de archivo persistence.xml

--- proyecto/src/main/java/com/hampcode/model/entity/Customer.java
+++ proyecto/src/main/java/com/hampcode/model/entity/Customer.java
@@ -1,5 +1,52 @@
 package com.hampcode.model.entity;
 
-public class Customer {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customers")
+public class Customer implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int Id;
+
+	@Column(name = "first_name", nullable = false, length = 50)
+	private String firstName;
+
+	@Column(name = "last_name", nullable = false, length = 50)
+	private String lastName;
+
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
 }
---
+++ proyecto/src/main/resources/META-INF/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO customers (first_name,last_name) VALUES ('Henry Antonio','Mendoza Puerta');
+INSERT INTO customers (first_name,last_name) VALUES ('Juan','Perez Rodriguez');
